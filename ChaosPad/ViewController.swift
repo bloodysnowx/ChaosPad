0e831f9bcff288e9c04bbea4bb0d0d5a3adbaea5
Atsushi Iwasa <a-iwasa@m3.com>
2016-05-02 14:20:17 +0900

    wip

--- ChaosPad/ViewController.swift
+++ ChaosPad/ViewController.swift
@@ -12,7 +12,7 @@ import OpenAL
 class ViewController: UIViewController {
     var device: COpaquePointer = nil
     var context: COpaquePointer = nil
-    let samplingRate = 22050
+    let samplingRate: ALsizei = 22050
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,16 +35,21 @@ class ViewController: UIViewController {
         alcMakeContextCurrent(context)
     }
     
+    private func hoge(x: ALsizei) -> ALshort {
+        return ALshort(sin(Double(x) * 3.14159 * 2 * 440 / Double(samplingRate)) * 32767)
+    }
+    
     @IBAction func play() {
         var buffer: ALuint = 0
         alGenBuffers(1, &buffer)
         var source: ALuint = 0
-        let x = 0..<samplingRate
-        let data: [ALshort] = (0..<samplingRate).map({ ALshort(sin(Double($0) * 3.14159 * 2 * 440 / samplingRate) * 32767) })
+        
+        let data: [ALshort] = (0..<samplingRate).map(hoge)
         alBufferData(buffer, AL_FORMAT_MONO16, data, ALsizei(sizeof(ALshort) * data.count), samplingRate)
         alGenSources(1, &source)
         alSourcei(source, AL_BUFFER, ALint(buffer))
         alSourcePlay(source)
+
     }
 }
 
